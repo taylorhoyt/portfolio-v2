e9ccf004bc89549f3f9af1f999acb4f11cd8aa84
Taylor Hoyt <taych2002@gmail.com>
2025-06-04 13:37:43 -0500

    Added hero section component

---
+++ src/components/sections/HeroSection.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+
+interface HeroSectionProps {
+  rightSide: () => React.ReactNode;
+  leftSide: () => React.ReactNode;
+  className?: string;
+}
+
+const HeroSection: FC<HeroSectionProps> = ({
+  rightSide,
+  leftSide,
+  className = "",
+}: HeroSectionProps) => {
+  return (
+    <section className={"h-fit w-full"}>
+      <div
+        className={`grid h-fit w-[1200px] max-w-[75vw] grid-cols-1 gap-4 lg:grid-cols-5 ${className}`}
+      >
+        <div className={"h-fit lg:col-span-3"}>{rightSide()}</div>
+        <div className={"h-fit lg:col-span-2"}>{leftSide()}</div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
