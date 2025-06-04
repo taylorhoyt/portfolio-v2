9bf5974194c7aa49b8a88883c859fd84029c46cb
Taylor Hoyt <taych2002@gmail.com>
2025-06-04 18:11:13 -0500

    Updated colors and added temporary profile pic

--- src/components/sections/TopSection.tsx
+++ src/components/sections/TopSection.tsx
@@ -16,8 +16,28 @@ const TopSection: FC<HeroSectionProps> = ({
       <div
         className={`grid h-fit w-[1200px] max-w-[75vw] grid-cols-1 gap-4 lg:grid-cols-5 ${className}`}
       >
-        <div className={"h-fit lg:col-span-3"}>{rightSide()}</div>
-        <div className={"h-fit lg:col-span-2"}>{leftSide()}</div>
+        <div className="bg-primary h-fit rounded-xl lg:col-span-3">
+          <div
+            className={
+              "from-electric-blue to-expressive-amethyst h-fit rounded-xl bg-linear-to-tl shadow-xl lg:col-span-3"
+            }
+          >
+            {rightSide()}
+          </div>
+          <div className="flex gap-2 p-4">
+            <div className="bg-expressive-red h-[35px] w-[35px] rounded-full"></div>
+            <div className="bg-bright-gold h-[35px] w-[35px] rounded-full"></div>
+            <div className="bg-expressive-blue h-[35px] w-[35px] rounded-full"></div>
+            <div className="bg-expressive-amethyst h-[35px] w-[35px] rounded-full"></div>
+          </div>
+        </div>
+        <div
+          className={
+            "from-electric-blue to-expressive-amethyst mx-auto h-fit w-fit rounded-xl bg-linear-to-tr lg:col-span-2 lg:row-span-2"
+          }
+        >
+          {leftSide()}
+        </div>
       </div>
     </section>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import TopSection from "@/components/sections/TopSection";
+import Image from "next/image";
 import React from "react";
 
 export default function Home() {
@@ -8,11 +9,35 @@ export default function Home() {
         <TopSection
           className={"m-auto"}
           rightSide={() => {
-            return <div className="bg-expressive-blue h-[200px] w-full"></div>;
+            return (
+              <div className="flex h-fit w-full flex-col rounded-xl p-4">
+                <h1 className="text-off-white text-3xl font-bold">
+                  Many things will be built
+                </h1>
+                <h1 className="text-expressive-blue pb-2 text-4xl font-bold underline underline-offset-4">
+                  in a real timeframe
+                </h1>
+                <h1 className="text-off-white text-3xl font-bold">
+                  Maybe even soon
+                </h1>
+                <h3 className="text-silver-gray text-wrap">
+                  Fullstack software engineer experienced in turning dreams into
+                  reality
+                </h3>
+              </div>
+            );
           }}
           leftSide={() => {
             return (
-              <div className="bg-secondary-background h-[200px] w-full"></div>
+              <div className="m-auto h-fit w-fit">
+                <Image
+                  src="/landing-images/alien.png"
+                  width={500}
+                  height={500}
+                  className="rounded-full p-6"
+                  alt="distinctly not me"
+                />
+              </div>
             );
           }}
         />
