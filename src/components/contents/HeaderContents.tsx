5fb8f4fec48229547ed344616ec919230e7aaf31
Taylor Hoyt <taych2002@gmail.com>
2025-06-05 16:39:17 -0500

    Integrated toggle button

--- src/components/contents/HeaderContents.tsx
+++ src/components/contents/HeaderContents.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import ThemeToggleButton from "../buttons/ThemeToggleButton";
 
 const HeaderContents = () => {
   return (
     <>
       <div>hamburger | name</div>
-      <div>toggle</div>
+      <ThemeToggleButton />
     </>
   );
 };
