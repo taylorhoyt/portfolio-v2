1ee39eb514e7db62f909a5b77b3a55625539f6ab
Taylor Hoyt <taych2002@gmail.com>
2025-06-05 16:39:06 -0500

    Integrated theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
