3affe954814b20f09c03dbeaa6b7e56c5470b53e
Taylor Hoyt <taych2002@gmail.com>
2025-06-06 16:17:22 -0500

    Changed icon sizes

--- src/components/buttons/ThemeToggleButton.tsx
+++ src/components/buttons/ThemeToggleButton.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Moon, Sun } from 'lucide-react';
-import { useTheme } from '@/context/ThemeContext';
-import BasicButton from './BasicButton';
+import React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "@/context/ThemeContext";
+import BasicButton from "./BasicButton";
 
 const ThemeToggleButton: React.FC = () => {
   const { theme, toggleTheme } = useTheme();
@@ -10,11 +10,11 @@ const ThemeToggleButton: React.FC = () => {
     <BasicButton
       onClick={toggleTheme}
       className="bg-secondary-background text-primary-text"
-      tooltipLabel={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
+      tooltipLabel={`Switch to ${theme === "light" ? "dark" : "light"} mode`}
     >
-      {theme === 'light' ? <Moon size={20} /> : <Sun size={20} />}
+      {theme === "light" ? <Moon size={28} /> : <Sun size={28} />}
     </BasicButton>
   );
 };
 
-export default ThemeToggleButton; 
+export default ThemeToggleButton;
