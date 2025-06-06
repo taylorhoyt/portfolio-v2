cb9e107f55aaf1fef7d51c57e5f1478ae353b1b5
Taylor Hoyt <taych2002@gmail.com>
2025-06-06 17:25:29 -0500

    Added title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,20 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import { ThemeProvider } from "@/context/ThemeContext";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Taylor Hoyt | Portfolio</title>
+        <meta name="description" content="Taylor Hoyt's Portfolio - Fullstack Software Engineer" />
+      </Head>
+      <ThemeProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
   );
 }
