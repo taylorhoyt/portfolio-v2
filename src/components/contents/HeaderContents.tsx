5ea7f466ac220bf0795d2d14429b60f21dd8ec4a
Taylor Hoyt <taych2002@gmail.com>
2025-06-11 15:32:55 -0500

    Made my name a link to go back to my main page

--- src/components/contents/HeaderContents.tsx
+++ src/components/contents/HeaderContents.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ThemeToggleButton from "../buttons/ThemeToggleButton";
 import MenuButton from "@/components/buttons/MenuButton";
+import Link from "next/link";
 
 const HeaderContents = () => {
   return (
@@ -8,7 +9,11 @@ const HeaderContents = () => {
       <div className="flex gap-4">
         <MenuButton />
         <div className="my-auto flex h-fit">
-          <h1 className="text-primary-text text-3xl font-bold">Taylor Hoyt</h1>
+          <Link href="/" rel="noopener noreferrer">
+            <h1 className="text-primary-text text-3xl font-bold">
+              Taylor Hoyt
+            </h1>
+          </Link>
         </div>
       </div>
       <ThemeToggleButton />
