66d6c56f50bbe7d9f1a930f3cdf7d1e50a04605f
Taylor Hoyt <taych2002@gmail.com>
2025-06-14 13:46:57 -0500

    Updated favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,32 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="msapplication-TileImage"
+          content="/favicon/mstile-144x144.png"
+        />
+      </Head>
       <body className="bg-primary-background antialiased">
         <Main />
         <NextScript />
