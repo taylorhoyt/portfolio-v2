e8d667e0dadf9076a12cbaa7ae20c5e64ce49203
Taylor Hoyt <taych2002@gmail.com>
2025-06-13 19:07:49 -0500

    Added separate file for timeline configuration

---
+++ src/data/timeline-entries.ts
@@ -0,0 +1,60 @@
+import { TimelineEntries } from "@/types/timeline";
+
+export const timelineEntries: TimelineEntries = {
+  timelineEntries: [
+    {
+      id: "1",
+      title: "Northwest Arkansas Community College",
+      subtitle: "Associates of Science",
+      startYear: 2020,
+      endYear: 2022,
+      description: "",
+      color: "#00402E",
+    },
+    {
+      id: "2",
+      title: "University of Arkansas",
+      subtitle: "Bachelor of Science in Computer Science",
+      startYear: 2022,
+      endYear: 2024,
+      description: "",
+      color: "#9D2235",
+    },
+    {
+      id: "3",
+      title: "Walmart Global Tech",
+      subtitle: "Software Engineer Intern",
+      startMonth: "May",
+      startYear: 2023,
+      endMonth: "August",
+      endYear: 2023,
+      description:
+        "During my internship at Walmart Global Tech, I focused on developing scalable Spring Boot APIs, ensuring they met enterprise standards for documentation, error handling, and performance. I actively collaborated with front-end teams to resolve integration challenges, which helped improve team agility and reduce delivery delays. I consistently prioritized writing testable and maintainable code and actively participated in Git workflows and code review practices.",
+      color: "#041F41",
+    },
+    {
+      id: "4",
+      title: "Kondux LLC",
+      subtitle: "Software Engineer",
+      startMonth: "November",
+      startYear: 2023,
+      endMonth: "July",
+      endYear: 2024,
+      description:
+        "As a Software Engineer at Kondux, I built the entire front-end for the Persona AI Portal, handling complex UI and wallet authentication, and developed the Disclosure Collection minting dApp, which generated approximately $200,000 in revenue. I also unified multiple web3 products into a single, modular platform, streamlining development. A key achievement was delivering a real-time 3D asset delivery proof-of-concept under tight deadlines, strengthening our partnership with NVIDIA.",
+      color: "#3D99B0",
+    },
+    {
+      id: "5",
+      title: "Walmart Global Tech",
+      subtitle: "Software Engineer II",
+      startMonth: "July",
+      startYear: 2024,
+      endMonth: "May",
+      endYear: 2025,
+      description:
+        "At Walmart Global Tech, I spearheaded a major overhaul of our Cypress automation suite, boosting test coverage from under 30% to over 80% and significantly improving test reliability. I also implemented robust monitoring and alerting systems for critical payment and returns middleware, ensuring quick issue resolution. A strong advocate for knowledge sharing, I led team-wide demos on automation and monitoring tools, earning recognition for my ownership, problem-solving skills, and consistent delivery of high-quality work.",
+      color: "#041F41",
+    },
+  ],
+};
