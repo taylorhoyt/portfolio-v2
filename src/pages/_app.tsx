423efcd8aa9e00bc0315ee825c305f0d41266dd6
Taylor Hoyt <taych2002@gmail.com>
2025-06-19 11:22:35 -0500

    Integrated vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,22 @@ import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import { ThemeProvider } from "@/context/ThemeContext";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Taylor Hoyt | Portfolio</title>
-        <meta name="description" content="Taylor Hoyt's Portfolio - Fullstack Software Engineer" />
+        <meta
+          name="description"
+          content="Taylor Hoyt's Portfolio - Fullstack Software Engineer"
+        />
       </Head>
       <ThemeProvider>
         <Layout>
           <Component {...pageProps} />
+          <Analytics />
         </Layout>
       </ThemeProvider>
     </>
